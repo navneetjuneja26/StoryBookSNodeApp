3d813231e41319ca2b5a7bd6d140ff88ba97df92
Navneet Juneja <navneetjuneja26@gmail.com>
2019-02-19 08:51:36 +0530

    Create keys_dev.js

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://navneetjuneja26:godisgreat84@ds231228.mlab.com:31228/storybooks-dev',
+    googleClientID:'602591976539-89e71nf3olaukcjtp8lb96tar58rh6gq.apps.googleusercontent.com',
+    googleClientSecret:'1K3c47W2EGbmizCToH8verVG'
+  };
